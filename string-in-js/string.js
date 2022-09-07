08e6c89ea768161ad1b4286a09e2a5355103cb5d
anannanur <anannaanu974@gmail.com>
2022-09-08 01:10:53 +0600

    string comparison

--- string-in-js/string.js
+++ string-in-js/string.js
@@ -10,3 +10,22 @@ console.log(String(a));
 console.log(a.toString());
 
 console.log(a + '');
+
+// string comparison 
+
+// string comparison happens by lexicographic order 
+// z,y,x...c,b,a,Z....A
+
+var lttr1 = 'abc';
+var lttr2 = 'aac';
+
+var num1 = 2;
+var num2 = '2';
+console.log(num1 === num2)
+
+console.log(lttr1 > lttr2);
+
+// comparing between string and number
+// here string is converted into number. then both r nmbr and eql
+console.log('001' == 1)
+
